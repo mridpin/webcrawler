5117437efaf262bfb976dc611563e750826cff96
Manuel Ridao Pineda <manuel.ridao@taiger.com>
2021-01-10 19:43:33 +0100

    added cors && mongo functionality

--- webcrawler-app/server.js
+++ webcrawler-app/server.js
@@ -1,15 +1,21 @@
 // Imports
 import Express from "Express"
+import Cors from "Cors"
+import mongodb from "mongodb"
 
 // Constants
-const app = Express();
+const app = new Express();
 const port = 8000;
+const mongoUrl = "mongodb://localhost:27017/jobsdb"
+const mongoClient = mongodb.MongoClient;
+// var db = null;
+var col = null
 
 // Configs
 app.use(Express.json());
+app.use(Cors()); // allow http from localhost
 
 // Endpoints
-
 // default endpoint to check if working
 app.get("/", (req, res) => {
     res.send("Hello world!");
@@ -18,32 +24,33 @@ app.get("/", (req, res) => {
 // GET endpoint to display data for all jobs (poll)
 app.get("/jobs", (req, res) => {
     var jobs = [];
-    // hardcoded for now
-    var job1 = {
-        id: 1,
-        url: "example.com", 
-        status: 0
-    };
-    jobs.push(job1);
-    var job2 = {
-        id: 2,
-        url: "google.com",
-        status: 1
-    };
-    jobs.push(job2);
-    var job3 = {
-        id: 3,
-        url: "wikipedia.com",
-        status: 2
-    };
-    jobs.push(job3);
-    res.send(jobs);
+    col.find({}).toArray( (err, result) => {
+        if (err) {
+            console.log("getJobs query error");
+            res.sendStatus(500);
+            throw err;
+        } else {
+            jobs.push(result); // try to concat here
+            console.log(`Returning ${jobs.length} jobs`);
+            res.send(result);
+        }
+    });
 });
 
 // POST endpoint to queue up a job
 app.post("/jobs", (req, res) => {
-    console.log(`Got body: ${req.body.targetUrl}`); // needs validation
-    res.sendStatus(200);
+    console.log(`Got body: ${req.body.targetJobUrl}`); // needs validation
+    col.insertOne(newJob, (err, result) => {
+        if (err) {
+            console.log("getJobs query error");
+            res.sendStatus(500);
+            throw err;
+        } else {
+            jobs.push(result); // try to concat here
+            console.log(`Returning ${jobs.length} jobs`);
+            res.sendStatus(200);
+        }
+    });
 });
 
 // GET to get results from 1 job
@@ -53,9 +60,55 @@ app.get("/jobs/:id", (req, res) => {
         id: 1,
         url: "example.com",
         results: ["example.com/uno", "example.com/dos"],
-        status: 1
+        status: 2
     };
     res.send(job);
+});
+
+// Database start
+mongoClient.connect(mongoUrl, (err, client) => {
+    if (err) {
+        console.log("MongoDB connection error!");
+        throw err;
+    } else {
+        console.log("MongoDB database created!");
+        // db = client.db("jobsdb");
+        col = client.db("jobsdb").collection("jobs");
+        // insert default data
+        // var job1 = {
+        //     id: 1,
+        //     url: "example.com", 
+        //     status: 0
+        // };
+        // var job2 = {
+        //     id: 2,
+        //     url: "google.com",
+        //     status: 1
+        // };
+        // var job3 = {
+        //     id: 3,
+        //     url: "wikipedia.com",
+        //     status: 2
+        // };
+        // col.insertOne(job1, (err, res) => {
+        //     if (err) {
+        //         throw err;
+        //     }
+        //     console.log(`Inserted job ${job1.id}`);
+        // });
+        // col.insertOne(job2, (err, res) => {
+        //     if (err) {
+        //         throw err;
+        //     }
+        //     console.log(`Inserted job ${job2.id}`);
+        // });
+        // col.insertOne(job3, (err, res) => {
+        //     if (err) {
+        //         throw err;
+        //     }
+        //     console.log(`Inserted job ${job3.id}`);
+        // });
+    }
 });
 
 // Server start
